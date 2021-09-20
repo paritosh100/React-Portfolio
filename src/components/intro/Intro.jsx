d1dd9917647feb19d8b5d852794db0d401f20c18
Paritosh Gandre <paritoshkrcg@gmail.com>
2021-09-20 20:11:55 +0530

    react portfolio

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,40 @@
+import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
+
+export default function Intro() {
+
+    const textRef = useRef();
+
+
+    useEffect(() => {
+        init(textRef.current, {
+             showCursor: false,
+             backDelay:1500,
+             backSpeed:60,
+             showCursor:true,
+              strings: ['Developer', 'Designer','Content Creater' ],
+             });
+
+        }, []);
+    return (
+        <div className="intro" id="intro">
+        <div className="left">
+
+            <div className="imgContainer">
+                <img src="assets/intro-image.png" alt="" />
+            </div>
+        </div>
+        <div className="right">
+            <div className="wrapper">
+                <h2>Hi There, I'm </h2>
+                <h1>Paritosh Gandre</h1>
+                <h3>Freelance <span ref={textRef}></span> </h3>
+            </div>
+            <a href="#portfolio">
+                <img src="assets/down.png" alt="" />
+            </a>
+        </div>
+        </div>
+    )
+}
